bf451e6d04a699cc9045627bb71e1e6ba5b9ab74
RickFqt <henriquefouquet@gmail.com>
2023-06-16 06:58:36 -0300

    Novo sprite de player

--- scripts/Control.gd
+++ scripts/Control.gd
@@ -1,14 +1,16 @@
 extends Node2D
 
-# Variável global para armazenar dados
+# Variáveis globais para armazenar dados
 var Control_iniciado: bool = false
 
+# TODO: inicializar as animações dos players direito
 var player1 = {
 	location = Vector2(0,0),
 	index_location = 0,
 	ob_coins = 0,
 	opt_coins = 0,
-	diplomas = 0
+	diplomas = 0,
+	animation = 0
 }
 
 var player2 = {
@@ -16,7 +18,8 @@ var player2 = {
 	index_location = 0,
 	ob_coins = 0,
 	opt_coins = 0,
-	diplomas = 0
+	diplomas = 0,
+	animation = 1
 }
 
 var semestre
@@ -25,26 +28,24 @@ var turno
 func inicializar_control(loc1, idx1, m_ob1, m_opt1, dip1, loc2, idx2, m_ob2, m_opt2, dip2, sem, turn, force := false):
 	if Control_iniciado == false or force:
 		
-		
+		# Atualiza player1
 		player1.location = loc1
 		player1.index_location = idx1
 		player1.ob_coins = m_ob1
 		player1.opt_coins = m_opt1
 		player1.diplomas = dip1
 		
-		
+		# Atualiza player2
 		player2.location = loc2
 		player2.index_location = idx2
 		player2.ob_coins = m_ob2
 		player2.opt_coins = m_opt2
 		player1.diplomas = dip2
 		
-		
+		# Atualiza o valor dos semestres e turnos
 		semestre = sem
 		turno = turn
 		Control_iniciado = true
-	
-# Função para trocar de cena
 
 func update_control(loc1, idx1, m_ob1, m_opt1, dip1, loc2, idx2, m_ob2, m_opt2, dip2, sem, turn):
 	inicializar_control(loc1, idx1, m_ob1, m_opt1, dip1, loc2, idx2, m_ob2, m_opt2, dip2, sem, turn, true)
--- scripts/TurnQueue.gd
+++ scripts/TurnQueue.gd
@@ -5,7 +5,7 @@ class_name TurnQueue
 
 @onready var control = get_node("/root/controle")
 var active_character
-var numero_turno = 0
+var numero_turno = 1
 var semestre_atual = 1
 var camera_path
 
@@ -16,22 +16,12 @@ func initialize():
 								$player1_table2.position, $player1_table2.curr_position, $player1_table2.moedas_obrigatorias, $player1_table2.moedas_optativas, $player1_table.n_diplomas,
 								semestre_atual, numero_turno)
 	else:
-		$player1_table.position = control.player1.location
-		$player1_table.curr_position = control.player1.index_location
-		$player1_table.moedas_obrigatorias = control.player1.ob_coins 
-		$player1_table.moedas_optativas = control.player1.opt_coins
-		$player1_table.n_diplomas = control.player1.diplomas
-		
-		$player1_table2.position = control.player2.location
-		$player1_table2.curr_position = control.player2.index_location
-		$player1_table2.moedas_obrigatorias = control.player2.ob_coins 
-		$player1_table2.moedas_optativas = control.player2.opt_coins
-		$player1_table2.n_diplomas = control.player2.diplomas
-		
-		semestre_atual = control.semestre
-		numero_turno = control.turno
-
+		load_game()
+	
+	get_child(0).player_number = 0
+	get_child(1).player_number = 1
 	active_character = get_child(0)
+	
 	await play_turn()
 	
 func play_turn():
@@ -52,13 +42,13 @@ func play_turn():
 		
 		# Verifica se todos os jogadores já jogaram
 		if(new_index == 0):
-			change_to_minigame()
 			
 			# Atualiza os dados da partida
-			numero_turno += 1
 			if(numero_turno % 3 == 0):
 				# O semestre trocou aeeee
 				semestre_atual += 1
+			numero_turno += 1
+			change_to_minigame()
 		else:
 			print("mudou o player!")
 			await play_turn()
@@ -66,13 +56,26 @@ func play_turn():
 		
 func change_to_minigame():
 	# ir pro minigame
+	save_game()
+	control.trocar_cena("res://levels/minigame_test.tscn")
+
+func load_game():
+	$player1_table.position = control.player1.location
+	$player1_table.curr_position = control.player1.index_location
+	$player1_table.moedas_obrigatorias = control.player1.ob_coins 
+	$player1_table.moedas_optativas = control.player1.opt_coins
+	$player1_table.n_diplomas = control.player1.diplomas
+	
+	$player1_table2.position = control.player2.location
+	$player1_table2.curr_position = control.player2.index_location
+	$player1_table2.moedas_obrigatorias = control.player2.ob_coins 
+	$player1_table2.moedas_optativas = control.player2.opt_coins
+	$player1_table2.n_diplomas = control.player2.diplomas
+	
+	semestre_atual = control.semestre
+	numero_turno = control.turno
+	
+func save_game():
 	control.update_control($player1_table.position, $player1_table.curr_position, $player1_table.moedas_obrigatorias, $player1_table.moedas_optativas, $player1_table.n_diplomas,
 						   $player1_table2.position, $player1_table2.curr_position, $player1_table2.moedas_obrigatorias, $player1_table2.moedas_optativas, $player1_table.n_diplomas,
 						   semestre_atual, numero_turno)
-	control.trocar_cena("res://levels/minigame_test.tscn")
-	#get_tree().change_scene_to_file("res://levels/minigame_test.tscn")
-	# voltar do mini game
-
-
-# TODO:
-# Voltar dos minigames!
