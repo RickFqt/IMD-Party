72dc2296e5dff78c399331e285290a45982199e2
Beatriz <72415136+beatrizgouveia03@users.noreply.github.com>
2023-06-17 23:40:49 -0300

    update selectionMenu

--- jogo_oficial/Scripts/player1.gd
+++ jogo_oficial/Scripts/player1.gd
@@ -5,8 +5,6 @@ var boy2 = "res://jogo_oficial/Players/boy2.tscn"
 var girl1 = "res://jogo_oficial/Players/girl1.tscn"
 var girl2 = "res://jogo_oficial/Players/girl2.tscn"
 
-var playerSelected
-
 func _input(event):
 	if event is InputEventKey:
 		if event.pressed:
@@ -16,26 +14,26 @@ func _input(event):
 				$"Girl 1/TextureRect3".visible = false
 				$"Girl 2/TextureRect4".visible = false
 				Global.player1Dir = boy1
-				Global.trocar_cena("res://jogo_oficial/Scenes/map.tscn")
+				
 			elif event.as_text_keycode() == "S":
 				$"Boy 1/TextureRect".visible = false
 				$"Boy 2/TextureRect2".visible = true
 				$"Girl 1/TextureRect3".visible = false
 				$"Girl 2/TextureRect4".visible = false
 				Global.player1Dir = boy2
-				Global.trocar_cena("res://jogo_oficial/Scenes/map.tscn")
+				
 			elif event.as_text_keycode() == "D":
 				$"Boy 1/TextureRect".visible = false
 				$"Boy 2/TextureRect2".visible = false
 				$"Girl 1/TextureRect3".visible = true
 				$"Girl 2/TextureRect4".visible = false
 				Global.player1Dir = girl1
-				Global.trocar_cena("res://jogo_oficial/Scenes/map.tscn")
+				
 			elif event.as_text_keycode() == "F":
 				$"Boy 1/TextureRect".visible = false
 				$"Boy 2/TextureRect2".visible = false
 				$"Girl 1/TextureRect3".visible = false
 				$"Girl 2/TextureRect4".visible = true
 				Global.player1Dir = girl2
-				Global.trocar_cena("res://jogo_oficial/Scenes/map.tscn")
+				
 
--- jogo_oficial/Scripts/player2.gd
+++ jogo_oficial/Scripts/player2.gd
@@ -1,11 +1,9 @@
 extends Control
 
-@onready var boy1 = $"Boy 1" #Obtenha o caminho para o botão A
-@onready var boy2 = $"Boy 2" #Obtenha o caminho para o botão B
-@onready var girl1 = $"Girl 1" #Obtenha o caminho para o botão C
-@onready var girl2 = $"Girl 2" #Obtenha o caminho para o botão D
-
-var playerSelected
+var boy1 = "res://jogo_oficial/Players/boy1.tscn"
+var boy2 = "res://jogo_oficial/Players/boy2.tscn"
+var girl1 = "res://jogo_oficial/Players/girl1.tscn"
+var girl2 = "res://jogo_oficial/Players/girl2.tscn"
 
 func _input(event):
 	if event is InputEventKey:
@@ -15,19 +13,23 @@ func _input(event):
 				$"Boy 2/TextureRect2".visible = false
 				$"Girl 1/TextureRect3".visible = false
 				$"Girl 2/TextureRect4".visible = false
+				Global.player1Dir = boy1
 			elif event.as_text_keycode() == "K":
 				$"Boy 1/TextureRect".visible = false
 				$"Boy 2/TextureRect2".visible = true
 				$"Girl 1/TextureRect3".visible = false
 				$"Girl 2/TextureRect4".visible = false
+				Global.player1Dir = boy2
 			elif event.as_text_keycode() == "L":
 				$"Boy 1/TextureRect".visible = false
 				$"Boy 2/TextureRect2".visible = false
 				$"Girl 1/TextureRect3".visible = true
 				$"Girl 2/TextureRect4".visible = false
+				Global.player1Dir = girl1
 			elif event.as_text_keycode() == "Ç":
 				$"Boy 1/TextureRect".visible = false
 				$"Boy 2/TextureRect2".visible = false
 				$"Girl 1/TextureRect3".visible = false
 				$"Girl 2/TextureRect4".visible = true
+				Global.player1Dir = girl2
 
